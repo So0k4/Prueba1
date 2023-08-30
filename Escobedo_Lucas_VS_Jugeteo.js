b429e235dd92aa5d032f4cb8cc289b8804ea1bda
So0k4 <143562529+So0k4@users.noreply.github.com>
2023-08-30 19:35:48 -0400

    Montaña rusa

---
+++ Escobedo_Lucas_VS_Jugeteo.js
@@ -0,0 +1,10 @@
+var alturaniño = 80;
+
+function elniñopuedesubir(){
+    if(alturaniño > 52)
+    console.log("¡subete chico!");
+else{
+    console.log("Lo siento, chico. Tal vez el proximo año.")
+}
+}
+elniñopuedesubir();
