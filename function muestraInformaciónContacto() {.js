db38bc6ddeda7db2dbc82af7a784ea208f0c7fe7
So0k4 <143562529+So0k4@users.noreply.github.com>
2023-08-30 19:42:47 -0400

    Contactos como zoro

---
+++ function muestraInformaciónContacto() {.js
@@ -0,0 +1,21 @@
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+// Paula, Smith, Calle principal 1234, St. Luis, MO, 12345
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+// frozen = brocoli, helado, croqueta de papa.
+
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+// Bambi, E.T., Toy Story, Zoro.
+
